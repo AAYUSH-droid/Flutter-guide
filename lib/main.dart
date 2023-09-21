db6006a8290b0b67468b1f076d3aa942008040af
aayush <sharma31aayush@gmail.com>
2023-09-21 16:39:24 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+// import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const RootPage(),
+    );
+  }
+}
+
+//StatefulWidget: Goal is that screen can be reloaded when user click on screen
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    //Scaffold contains a appbar, body and navigaton bar
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
